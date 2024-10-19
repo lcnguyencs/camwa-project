bbc2f63c0a9162d74ac82f9d58549964525942ba
Dang Trung Hieu <94620323+Dangtrunghieu2002@users.noreply.github.com>
2024-10-19 14:02:44 +0700

    update database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,6 +3,12 @@ require('dotenv').config();
 
 const { Sequelize } = require('sequelize');
 
+console.log(process.env.DB_NAME)
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASSWORD)
+console.log(process.env.DB_HOST)
+console.log(process.env.DB_PORT)
+
 // Initialize Sequelize with environment variables
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
