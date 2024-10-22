99aab280227b4fd8c3e212e9199b5fcb5581bf06
Tho_NguyenDuc <ductho892000@gmail.com>
2024-10-22 14:59:02 +0700

    Angular update 22.10

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  template: `<h1>Hello world!</h1>`,
+})
+export class AppComponent {
+  title = "homes";
+}
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   storageBucket: "vgu-attendance-management.appspot.com",
   messagingSenderId: "251948816899",
   appId: "1:251948816899:web:c8e6124bfb11bf8a914c36",
-  measurementId: "G-W39BVMLVST"
+  measurementId: "G-W39BVMLVST",
 };
 
 // Initialize Firebase
